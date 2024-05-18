1d283cde6fa7624a056585e6e7c0fd5cc6a8bab1
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-05-18 10:23:46 +0900

    fix: remove domain setting

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,18 +1,8 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { Stack } from "aws-cdk-lib";
-import * as cognito from "aws-cdk-lib/aws-cognito";
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
-});
-
-const { cfnUserPool } = backend.auth.resources.cfnResources;
-const authStack = Stack.of(cfnUserPool);
-
-new cognito.CfnUserPoolDomain(authStack, "Domain", {
-  userPoolId: cfnUserPool.ref,
-  domain: "moonlighting",
 });
